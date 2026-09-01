6df3fb32fc8b9d3d248d8169a5c10ad16f509a67
Vitor George <vitor.george@gmail.com>
2026-09-01 11:40:13 +0100

    feat: adopt Gallery on /training

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -1,44 +1,20 @@
-import { CardDetailed } from "@teamimpact/veda-ui-blocks";
-import { PageMasthead, Section } from "@/app/components";
+import { Suspense } from "react";
+import { Gallery, PageMasthead, Section } from "@/app/components";
+import { toGalleryItem } from "@/app/components/Gallery/Gallery.helpers";
 import { TRAININGS, TRAININGS_EXTERNAL } from "@/app/site-config/training";
 import { TRAINING_CARD_MASTHEAD } from "@/app/site-config/training/toplevel-page__card-masthead";
-import { makeCardDetailedProps, makeCardMastHeadProps } from "../site-config/content.helpers";
+import { makeCardMastHeadProps } from "../site-config/content.helpers";
 
 export default function TrainingCollectionPage() {
+  const items = [...TRAININGS, ...TRAININGS_EXTERNAL].map(toGalleryItem);
+
   return (
     <>
       <PageMasthead {...makeCardMastHeadProps(TRAINING_CARD_MASTHEAD)} />
       <Section>
-        <div className="grid-row grid-gap">
-          {[...TRAININGS, ...TRAININGS_EXTERNAL].map(
-            ({
-              id,
-              contentType,
-              title,
-              description,
-              thumbnailImage,
-              categories,
-              themes,
-              ...rest
-            }) => (
-              <div key={id} className="grid-col-12 tablet:grid-col-6 margin-y-1 desktop:margin-y-2">
-                <CardDetailed
-                  {...makeCardDetailedProps({
-                    id,
-                    contentType,
-                    title,
-                    description,
-                    thumbnailImage,
-                    categories,
-                    themes,
-                    url: "url" in rest ? rest.url : undefined,
-                  })}
-                  className="height-card-lg"
-                />
-              </div>
-            ),
-          )}
-        </div>
+        <Suspense>
+          <Gallery items={items} />
+        </Suspense>
       </Section>
     </>
   );
