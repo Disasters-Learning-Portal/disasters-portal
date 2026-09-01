a7dc7f1e6b382524d748047588fbe04ff2480886
Vitor George <vitor.george@gmail.com>
2026-09-01 11:41:00 +0100

    feat: adopt Gallery on /data-gallery

--- app/data-gallery/page.tsx
+++ app/data-gallery/page.tsx
@@ -1,9 +1,7 @@
-import { CardDetailed } from "@teamimpact/veda-ui-blocks";
-import { PageMasthead, Section } from "@/app/components";
-import {
-  makeCardDetailedImageLeftProps,
-  makeCardMastHeadProps,
-} from "@/app/site-config/content.helpers";
+import { Suspense } from "react";
+import { Gallery, PageMasthead, Section } from "@/app/components";
+import { toGalleryItem } from "@/app/components/Gallery/Gallery.helpers";
+import { makeCardMastHeadProps } from "@/app/site-config/content.helpers";
 import { DATA } from "@/app/site-config/data";
 import { DATA_GALLERY_CARD_MASTHEAD } from "@/app/site-config/data/toplevel-page__card-masthead";
 
@@ -12,26 +10,9 @@ export default function DataGalleryPage() {
     <>
       <PageMasthead {...makeCardMastHeadProps(DATA_GALLERY_CARD_MASTHEAD)} />
       <Section>
-        <div className="grid-row grid-gap">
-          {DATA.map(
-            ({ id, contentType, title, description, thumbnailImage, categories, themes }) => (
-              <div key={id} className="grid-col-12 margin-y-1 desktop:margin-y-2">
-                <CardDetailed
-                  {...makeCardDetailedImageLeftProps({
-                    id,
-                    contentType,
-                    title,
-                    description,
-                    thumbnailImage,
-                    categories,
-                    themes,
-                  })}
-                  className="height-card-sm"
-                />
-              </div>
-            ),
-          )}
-        </div>
+        <Suspense>
+          <Gallery items={DATA.map(toGalleryItem)} />
+        </Suspense>
       </Section>
     </>
   );
