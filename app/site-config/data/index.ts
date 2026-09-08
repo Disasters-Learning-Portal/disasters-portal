cbc9d187a9b1da3f9e4175869074b743c7531dd8
Andrew Blackford <62357205+acblackford@users.noreply.github.com>
2026-09-08 10:44:03 -0500

    add opera to data gallery list

--- app/site-config/data/index.ts
+++ app/site-config/data/index.ts
@@ -3,6 +3,7 @@ import type { DataContent } from "@/app/site-config/types";
 import { DATA__BLACK_MARBLE_BLUE_YELLOW } from "./data__black-marble-blue-yellow";
 import { DATA__BLACK_MARBLE_HD } from "./data__black-marble-hd";
 import { DATA__GAIA } from "./data__gaia";
+import { DATA__OPERA_DISP_S1_COHERENCE } from "./data__opera-disp-s1-coherence";
 import { DATA__OPERA_DIST_S1 } from "./data__opera-dist-s1";
 import { DATA__SENTINEL_2_COLOR_INFRARED } from "./data__sentinel-2-color-infrared";
 import { DATA__SENTINEL_2_DNBR } from "./data__sentinel-2-dnbr";
@@ -15,6 +16,7 @@ export const DATA: DataContent[] = [
   DATA__BLACK_MARBLE_BLUE_YELLOW,
   DATA__BLACK_MARBLE_HD,
   DATA__GAIA,
+  DATA__OPERA_DISP_S1_COHERENCE,
   DATA__OPERA_DIST_S1,
   DATA__SENTINEL_2_TRUE_COLOR,
   DATA__SENTINEL_2_COLOR_INFRARED,
