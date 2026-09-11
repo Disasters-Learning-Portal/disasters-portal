780017ed9b99780ca243663c9dcb92807a5b7abc
Andrew Blackford <62357205+acblackford@users.noreply.github.com>
2026-09-11 13:03:07 -0500

    create 6 landsat and 2 s2 dataset gallery pages

---
+++ app/site-config/data/data__landsat-true-color.ts
@@ -0,0 +1,88 @@
+import type { DataContent } from "@/app/site-config/types";
+
+export const DATA__LANDSAT_TRUE_COLOR: DataContent = {
+  id: "landsat-true-color",
+
+  contentType: "data",
+
+  title: "Landsat True Color Imagery",
+
+  description:
+    "The Landsat True Color RGB composite provides a natural-looking view of the Earth's surface similar to how it would appear to the human eye.",
+
+  thumbnailImage: {
+    src: "/img/data/landsat-true-color.webp",
+    alt: "Landsat True Color imagery example",
+  },
+
+  mastheadImage: {
+    src: "/img/data/landsat-true-color.webp",
+    alt: "Landsat True Color imagery example",
+  },
+
+  themes: ["respond", "build", "prepare", "recover"],
+
+  categories: [
+    "severewx",
+    "fire",
+    "heat",
+    "flood",
+    "tropical cyclone",
+    "earthquake",
+    "winter weather",
+  ],
+
+  relatedContent: ["landsat-natural-color", "landsat-color-infrared", "sentinel-2-true-color"],
+
+  body: [
+    {
+      type: "text",
+      heading: "Summary",
+      headingLevel: "h3",
+      paragraphs: [
+        "The Landsat True Color RGB composite provides a natural-looking representation of the Earth's surface using the red, green, and blue visible wavelength bands. For Landsat 8 and Landsat 9, the composite is created using Operational Land Imager (OLI) Bands 4, 3, and 2 for red, green, and blue, respectively.",
+      ],
+    },
+
+    {
+      type: "text",
+      heading: "Suggested Use",
+      headingLevel: "h3",
+      paragraphs: [
+        "Landsat True Color imagery can be used for visual interpretation of surface conditions before, during, and after natural hazards. The imagery can support assessment of wildfire impacts, flooding, storm damage, changes to vegetation and land cover, and other visible surface changes. Because the composite uses visible wavelengths, clouds, smoke, haze, and atmospheric conditions may obscure the land surface.",
+      ],
+    },
+
+    {
+      type: "text",
+      heading: "Satellite/Sensor",
+      headingLevel: "h3",
+      paragraphs: [
+        "Operational Land Imager (OLI) and Operational Land Imager-2 (OLI-2) aboard the NASA/USGS Landsat 8 and Landsat 9 satellites",
+      ],
+    },
+
+    {
+      type: "text",
+      heading: "Resolution",
+      headingLevel: "h3",
+      paragraphs: ["30 meters"],
+    },
+
+    {
+      type: "text",
+      heading: "Credits",
+      headingLevel: "h3",
+      paragraphs: ["NASA, U.S. Geological Survey (USGS)"],
+    },
+
+    {
+      type: "text",
+      heading: "Tags",
+      headingLevel: "h3",
+      paragraphs: [
+        "NASA, USGS, Landsat, Landsat 8, Landsat 9, OLI, OLI-2, True Color, Optical",
+      ],
+    },
+  ],
+};
--- app/site-config/data/index.ts
+++ app/site-config/data/index.ts
@@ -6,12 +6,20 @@ import { DATA__BLACK_MARBLE_BLUE_YELLOW } from "./data__black-marble-blue-yellow
 import { DATA__BLACK_MARBLE_BRDF } from "./data__black-marble-brdf";
 import { DATA__BLACK_MARBLE_HD } from "./data__black-marble-hd";
 import { DATA__GAIA } from "./data__gaia";
+import { DATA__LANDSAT_COLOR_INFRARED } from "./data__landsat-color-infrared";
+import { DATA__LANDSAT_MNDWI } from "./data__landsat-mndwi";
+import { DATA__LANDSAT_NATURAL_COLOR } from "./data__landsat-natural-color";
+import { DATA__LANDSAT_NBR } from "./data__landsat-nbr";
+import { DATA__LANDSAT_NDVI } from "./data__landsat-ndvi";
+import { DATA__LANDSAT_TRUE_COLOR } from "./data__landsat-true-color";
 import { DATA__OPERA_DISP_S1_COHERENCE } from "./data__opera-disp-s1-coherence";
 import { DATA__OPERA_DIST_S1 } from "./data__opera-dist-s1";
 import { DATA__SENTINEL_1_SENTINEL_2_BURN_SEVERITY } from "./data__sentinel-1-sentinel-2-burn-severity";
 import { DATA__SENTINEL_2_COLOR_INFRARED } from "./data__sentinel-2-color-infrared";
 import { DATA__SENTINEL_2_DNBR } from "./data__sentinel-2-dnbr";
+import { DATA__SENTINEL_2_MNDWI } from "./data__sentinel-2-mndwi";
 import { DATA__SENTINEL_2_NBR } from "./data__sentinel-2-nbr";
+import { DATA__SENTINEL_2_NDVI } from "./data__sentinel-2-ndvi";
 import { DATA__SENTINEL_2_SWIR } from "./data__sentinel-2-swir";
 import { DATA__SENTINEL_2_TRUE_COLOR } from "./data__sentinel-2-true-color";
 import { DATA__UMBRA_SIGMA_NAUGHT } from "./data__umbra_sigma_naught";
@@ -23,14 +31,22 @@ export const DATA: DataContent[] = [
   DATA__BLACK_MARBLE_BRDF,
   DATA__BLACK_MARBLE_HD,
   DATA__GAIA,
+  DATA__LANDSAT_COLOR_INFRARED,
+  DATA__LANDSAT_MNDWI,
+  DATA__LANDSAT_NATURAL_COLOR,
+  DATA__LANDSAT_NBR,
+  DATA__LANDSAT_NDVI,
+  DATA__LANDSAT_TRUE_COLOR,
   DATA__OPERA_DISP_S1_COHERENCE,
   DATA__OPERA_DIST_S1,
   DATA__SENTINEL_1_SENTINEL_2_BURN_SEVERITY,
-  DATA__SENTINEL_2_TRUE_COLOR,
   DATA__SENTINEL_2_COLOR_INFRARED,
-  DATA__SENTINEL_2_SWIR,
-  DATA__SENTINEL_2_NBR,
   DATA__SENTINEL_2_DNBR,
+  DATA__SENTINEL_2_MNDWI,
+  DATA__SENTINEL_2_NBR,
+  DATA__SENTINEL_2_NDVI,
+  DATA__SENTINEL_2_SWIR,
+  DATA__SENTINEL_2_TRUE_COLOR,
   DATA__UMBRA_SIGMA_NAUGHT,
 ];
 
