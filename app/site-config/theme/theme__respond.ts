7fc9466b0d8ddb8a7b8fd7352880827fe360fbe8
Kyle Lesinger <kylelesinger@gmail.com>
2026-09-15 16:34:15 -0500

    fix(respond): credit Copernicus Sentinel-2 in the data visualization caption

--- app/site-config/theme/theme__respond.ts
+++ app/site-config/theme/theme__respond.ts
@@ -44,7 +44,7 @@ export const RESPOND_CONTENT: ThemeContent = {
         dateRange: { from: "2019-03-16", to: "2019-03-16" },
       },
       caption:
-        "The midwestern United States was greatly impacted by flooding during Spring 2019. This visualization shows how flooding can be detected using moderate resolution ESA Sentinel-2 imagery, comparing a True Color composite from Sentinel-2 imagery on the left with a Modified Normalized Difference Water Index (mNDWI) product derived from Sentinel-2 imagery on the right. In the mNDWI product, likely water appears as blue.",
+        "The midwestern United States was greatly impacted by flooding during Spring 2019. This visualization shows how flooding can be detected using moderate resolution Copernicus Sentinel-2 imagery, comparing a True Color composite from Sentinel-2 imagery on the left with a Modified Normalized Difference Water Index (mNDWI) product derived from Sentinel-2 imagery on the right. In the mNDWI product, likely water appears as blue.",
     },
   ],
 } as const;
