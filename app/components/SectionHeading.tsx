7eb80cdc188973b7669472ff338c95aca35d2add
Simi Damani <simi@developmentseed.org>
2026-09-23 15:21:07 -0700

    fix: SectionHeading - update alignment to match figma

--- app/components/SectionHeading.tsx
+++ app/components/SectionHeading.tsx
@@ -9,8 +9,8 @@ type SectionHeadingProps = ComponentProps<"h2"> & {
 
 export const SectionHeading = ({ href, children, className, ...props }: SectionHeadingProps) => {
   return (
-    <div className="display-flex flex-justify flex-align-center">
-      <h2 className={`${className ?? ""} font-sans-2xl padding-bottom-2 margin-0`} {...props}>
+    <div className="display-flex flex-justify flex-align-end margin-bottom-105">
+      <h2 className={`${className ?? ""} font-sans-2xl margin-0`} {...props}>
         {children}
       </h2>
       {href && (
