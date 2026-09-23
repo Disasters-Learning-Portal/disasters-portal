d2fc024c95f945e080af611f5a012e4d40fab5a3
Wille Marcel <wille.yyz@gmail.com>
2026-09-23 16:37:40 -0300

    Adjust margin

--- app/site-config/header.tsx
+++ app/site-config/header.tsx
@@ -38,7 +38,7 @@ export const MOCK_HEADER_PROPS: HeaderProps = {
     logo: <AppImage src="/img/nasa-logo.svg" alt="NASA Logo" width={55} height={55} />,
     url: "/",
     title: (
-      <span className="display-flex flex-column font-family-serif text-uppercase">
+      <span className="margin-left-neg-2 display-flex flex-column font-family-serif text-uppercase">
         <span className="text-bold">Disasters</span>
         <span className="text-light">Program</span>
       </span>
