ba83ee0cbe2fc21c6d1811bf6dcb7672e9bf154f
ethankerrwx <140041545+ethankerrwx@users.noreply.github.com>
2026-09-24 14:39:30 -0500

    trying different dates

--- app/site-config/datastory/datastory__tools-you-can-use-mapping-flood-impacts.ts
+++ app/site-config/datastory/datastory__tools-you-can-use-mapping-flood-impacts.ts
@@ -41,7 +41,7 @@ export const DATASTORY__TOOLS_YOU_CAN_USE_MAPPING_FLOOD_IMPACTS: DataStoryConten
         type: "raster",
         collectionId: "sentinel1-hydrosar-wm-iw-dvr-subdaily",
         collectionAssetId: "dvr",
-        dateRange: { from: "2024-11-23", to: "2024-11-24" },
+        dateRange: { from: "2024-11-01", to: "2024-11-23" },
       },
       caption:
         "The Copernicus Sentinel-1 satellite collected synthetic aperture radar (SAR) imagery over the southeast and central U.S. from April 4-9, 2025, capturing flooding from severe storms. HydroSAR surface water extents are shown in blue. These are overlaid on VH cross-pol SAR imagery (grayscale), in which still water and other flat surfaces appear black, while diffuse scatterers like vegetation appear in lighter shades. Credits: NASA Disasters Program, Alaska Satellite Facility. Contains modified Copernicus Sentinel data (2025) processed by ESA.",
