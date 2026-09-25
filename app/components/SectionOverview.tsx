9789609cb179e2cdf78a86c1fbf2357d80d1d76b
Simi Damani <simi@developmentseed.org>
2026-09-24 17:24:14 -0700

    fix: SectionOverview, simplify code

--- app/components/SectionOverview.tsx
+++ app/components/SectionOverview.tsx
@@ -1,33 +1,36 @@
+import type { LinkProps } from "@teamimpact/veda-ui-blocks";
 import { Section, type SectionProps } from "@/app/components/Section";
-import type { SectionOverviewItemProps } from "@/app/site-config/event/event.helpers";
+import { type AppLink, AppLinkStyled } from "./AppLink";
 
-const ContainerItem = ({ title, content }: { title: string; content: React.ReactNode }) => (
-  <div className="margin-right-8 margin-bottom-2">
-    <div className="font-ui-3xs line-height-ui-5 text-bold text-uppercase text-ls-1 text-base-dark margin-bottom-05">
-      {title}
-    </div>
-    {content}
-  </div>
-);
+type SectionOverviewProps = Omit<SectionProps, "children"> & {
+  items: {
+    label: string;
+    link: Omit<LinkProps<typeof AppLink>, "color" | "size" | "variant" | "children"> & {
+      label: string;
+    };
+  }[];
+};
 
-type SectionOverviewProps = SectionProps & SectionOverviewItemProps;
-
-export const SectionOverview = ({
-  overviewItems,
-  className = "",
-  children,
-  ...rest
-}: SectionOverviewProps) => {
-  if (overviewItems.length === 0) return null;
+export const SectionOverview = ({ items, className = "", ...rest }: SectionOverviewProps) => {
+  if (items.length === 0) return null;
 
   return (
     <Section className={`padding-bottom-3 ${className}`} {...rest}>
-      <div className="display-flex flex-wrap border-top border-bottom border-base-lighter padding-top-3 padding-bottom-1">
-        {overviewItems.map((i) => (
-          <ContainerItem key={i.title} {...i} />
+      <div
+        className="display-flex flex-wrap border-top border-bottom border-base-lighter padding-y-3"
+        style={{ gap: "4rem" }}
+      >
+        {items.map(({ label, link: { label: linkLabel, ...linkProps } }) => (
+          <div key={label}>
+            <div className="font-ui-3xs line-height-ui-5 text-bold text-uppercase text-ls-1 text-base-dark margin-bottom-05">
+              {label}
+            </div>
+            <AppLinkStyled {...linkProps} variant="text">
+              {linkLabel}
+            </AppLinkStyled>
+          </div>
         ))}
       </div>
-      {children}
     </Section>
   );
 };
--- app/site-config/event/event.helpers.tsx
+++ app/site-config/event/event.helpers.tsx
@@ -1,4 +1,4 @@
-import { type CardProps, Link } from "@teamimpact/veda-ui-blocks";
+import type { CardProps } from "@teamimpact/veda-ui-blocks";
 import type { AppLink } from "@/app/components/AppLink";
 import type { SidebarDetail } from "@/app/components/PageSidebar";
 import {
@@ -48,35 +48,31 @@ export const transformEventToPageSidebarDetails = (event: EventContent): Sidebar
   ];
 };
 
-export type SectionOverviewItemProps = {
-  overviewItems: { title: string; content: React.ReactNode }[];
-};
-
-export const transformEventToSectionOverviewProps = (
-  event: EventContent,
-): SectionOverviewItemProps => {
+export const transformEventToSectionOverviewProps = (event: EventContent) => {
   const { linkDHSFEMA, linkUSGovernment } = event;
 
   return {
-    overviewItems: [
+    items: [
       linkUSGovernment
         ? {
-            title: "What the U.S. government is doing",
-            content: (
-              <Link variant="text" className="text-bold" isExternal href={linkUSGovernment.href}>
-                {linkUSGovernment.label ?? "Read more"}
-              </Link>
-            ),
+            label: "What the U.S. government is doing",
+            link: {
+              className: "text-bold",
+              isExternal: true,
+              href: linkUSGovernment.href,
+              label: linkUSGovernment.label ?? "Read more",
+            },
           }
         : null,
       linkDHSFEMA
         ? {
-            title: "What DHS and FEMA are doing",
-            content: (
-              <Link variant="text" className=" text-bold" isExternal href={linkDHSFEMA.href}>
-                {linkDHSFEMA.label ?? "Read more"}
-              </Link>
-            ),
+            label: "What DHS and FEMA are doing",
+            link: {
+              className: "text-bold",
+              isExternal: true,
+              href: linkDHSFEMA.href,
+              label: linkDHSFEMA.label ?? "Read more",
+            },
           }
         : null,
     ].filter((item): item is NonNullable<typeof item> => item !== null),
