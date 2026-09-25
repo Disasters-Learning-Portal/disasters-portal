8af063a88fc44aaf12d27ee873b80bc588a7e9d7
Simi Damani <simi@developmentseed.org>
2026-09-25 16:53:30 -0700

    fix: section heading - update to accept titleAs prop to specific heading level

--- app/components/SectionHeading.tsx
+++ app/components/SectionHeading.tsx
@@ -3,21 +3,25 @@
 import type { ComponentProps } from "react";
 import { type AppLink, AppLinkStyled } from "@/app/components/AppLink";
 
-type SectionHeadingProps = ComponentProps<"h2"> & {
+type SectionTitle = React.HTMLAttributes<HTMLHeadingElement> & {
+  titleAs?: "h1" | "h2" | "h3" | "h4";
   linkProps?: { label?: string; href?: ComponentProps<typeof AppLink>["href"] };
 };
 
 export const SectionHeading = ({
   linkProps: { href, label = "View All" } = {},
+  titleAs = "h2",
   children,
   className,
   ...props
-}: SectionHeadingProps) => {
+}: SectionTitle) => {
+  const TitleAs = titleAs;
+
   return (
     <div className="display-flex flex-justify flex-align-end margin-bottom-105">
-      <h2 className={`${className ?? ""} font-sans-2xl margin-0`} {...props}>
+      <TitleAs className={`font-sans-2xl margin-0 ${className ?? ""}`} {...props}>
         {children}
-      </h2>
+      </TitleAs>
       {href && (
         <AppLinkStyled href={href} variant="arrow" color="secondary">
           {label}
