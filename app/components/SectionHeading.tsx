7e58821c11f116c9a32afb9775e895f55dbc62cb
Simi Damani <simi@developmentseed.org>
2026-09-25 16:56:40 -0700

    fix: rename titleAs to be consistent with SectionHeading component

--- app/components/SectionHeading.tsx
+++ app/components/SectionHeading.tsx
@@ -4,24 +4,24 @@ import type { ComponentProps } from "react";
 import { type AppLink, AppLinkStyled } from "@/app/components/AppLink";
 
 type SectionTitle = React.HTMLAttributes<HTMLHeadingElement> & {
-  titleAs?: "h1" | "h2" | "h3" | "h4";
+  headingAs?: "h1" | "h2" | "h3" | "h4";
   linkProps?: { label?: string; href?: ComponentProps<typeof AppLink>["href"] };
 };
 
 export const SectionHeading = ({
   linkProps: { href, label = "View All" } = {},
-  titleAs = "h2",
+  headingAs = "h2",
   children,
   className,
   ...props
 }: SectionTitle) => {
-  const TitleAs = titleAs;
+  const HeadingAs = headingAs;
 
   return (
     <div className="display-flex flex-justify flex-align-end margin-bottom-105">
-      <TitleAs className={`font-sans-2xl margin-0 ${className ?? ""}`} {...props}>
+      <HeadingAs className={`font-sans-2xl margin-0 ${className ?? ""}`} {...props}>
         {children}
-      </TitleAs>
+      </HeadingAs>
       {href && (
         <AppLinkStyled href={href} variant="arrow" color="secondary">
           {label}
